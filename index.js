883afd5f4c4ba0a2effddb385d17b62c9884bbe3
Diego <diekus@gmail.com>
2019-06-19 09:15:47 +0100

    Separated Server + Client

--- index.js
+++ index.js
@@ -12,8 +12,8 @@ io.on('connection', function(socket){
         console.log('user disconnected');
     });
     socket.on('chat message', function(msg){
-        console.log('message: ' + msg);
         io.emit('chat message', msg); //sends the message to everyone
+        console.log('message: ' + msg);
     });
 });
 
