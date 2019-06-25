a2f8cd85daac076fa1adadea6a58db5445219311
Diego <diekus@gmail.com>
2019-06-25 11:51:51 +0100

    basic star creation
    -creates stars upon reaching page

--- js/client.js
+++ js/client.js
@@ -1,9 +1,26 @@
-window.addEventListener('DOMContentLoaded', function(){});
 /* SOCKET SETUP */
-var socket = io('localhost:3000'); // socket.emit('chat message', 'msg');
-socket.on('chat message', function(msg){
-    console.log(msg);
+const socket = io('http://localhost:3000');
+let stars = [];
+
+socket.on('star-connected', data => {
+    stars = data;
+    console.log(`you (${stars[stars.length - 1].id}) are now connected`);
+    console.log(data);
+    stars.forEach(element => {
+        createStar(element.id, element.r, element. x, element.y, element.z);        
+    });
 });
+
+socket.on('new-star-joined', newStar => {
+    console.log(`star ${newStar.id} joined`);
+    createStar(newStar.id, newStar.r, newStar.x, newStar.y, newStar.z);
+});
+
+socket.on('star-disconnected', usrId => {
+    deleteStar(usrId);
+    console.log(`star ${usrId} disconnected`);
+});
+
 /* BABYLON SETUP */
 const canvas = document.getElementById('renderCanvas');
 const engine = new BABYLON.Engine(canvas, true);
@@ -14,7 +31,7 @@ let createScene = function(){
     camera.setTarget(BABYLON.Vector3.Zero());
     camera.attachControl(canvas, false);
     let light = new BABYLON.HemisphericLight('light1', new BABYLON.Vector3(0,1,0), scene);
-    let ground = BABYLON.Mesh.CreateGround('ground1', 6, 6, 2, scene);
+    //let ground = BABYLON.Mesh.CreateGround('ground1', 6, 6, 2, scene);
     return scene;
 }
 
@@ -27,6 +44,17 @@ window.addEventListener('resize', function() { //resize the window
 });
 
 /* PLANETS APP */
-let createStar = function(r, x, y, z,) {
-
+let createStar = function(id, r, x, y, z) {
+    let sphere = BABYLON.MeshBuilder.CreateSphere(id, {segments: 8, diameter: r}, scene);
+    sphere.position = new BABYLON.Vector3(x, y, z);
 }
+
+let deleteStar = id => {
+    for(i = 0; i < stars.length; i++) {
+        if(stars[i].id == id) {
+            stars.splice(i, 1);
+            break;
+        } 
+    }
+    //TODO: Deletes from BabylonJS scene
+};
--- server.js
+++ server.js
@@ -1,22 +1,43 @@
-let app = require('express')();
-let http = require('http').createServer(app);
-let io = require('socket.io')(http);
+const io = require('socket.io')(3000); //npm run devStart
 
-app.get('/', function(req, res){
-    res.sendFile(__dirname + '/index.html');
-});
+io.on('connection', socket => {
+    let protostar = createStar(socket.id);
+    stars.push(protostar);
+    console.log(`star ${socket.id} connected. # Stars: ${stars.length}`);
+    socket.emit('star-connected', stars); //sends existing stars INCLUDING self to paint
 
-io.on('connection', function(socket){
-    console.log('a user connected');
-    socket.on('disconnect', function(){
-        console.log('user disconnected');
+    socket.broadcast.emit('new-star-joined', protostar); //lets others know theres a new star to create
+
+    socket.on('disconnect', () => {
+        socket.broadcast.emit('star-disconnected', socket.id);
+        deleteStar(socket.id);
+        console.log(`star ${socket.id} disconnected. # Stars: ${stars.length}`);
     });
-    socket.on('chat message', function(msg){
-        io.emit('chat message', msg); //sends the message to everyone
-        console.log('message: ' + msg);
-    });
+
 });
 
-http.listen(3000, function() {
-    console.log('listening on *:3000');
-});
+/* SPACE SETUP */
+let xConfines = 20;
+let yConfines = 20;
+let zConfines = 20;
+let initialMaxRadius = .7;
+let stars = []; //contains all stars in the scene
+
+let createStar = id => {
+    let newStarData = {
+        id: id,
+        r: Math.random() * initialMaxRadius,
+        x: Math.random() * xConfines,
+        y: Math.random() * yConfines,
+        z: Math.random() * zConfines}
+    return newStarData;
+};
+
+let deleteStar = id => {
+    for(i = 0; i < stars.length; i++) {
+        if(stars[i].id == id) {
+            stars.splice(i, 1);
+            break;
+        } 
+    }
+};
