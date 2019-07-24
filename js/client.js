ba6cc82bf982d5838980892edcb8909a08d2e3fa
Diego <diekus@gmail.com>
2019-07-24 19:18:47 +0100

    planets draft

--- js/client.js
+++ js/client.js
@@ -1,13 +1,21 @@
-/* SOCKET SETUP */
-const socket = io('http://localhost:3000');
+/*
+*
+*
+SOCKET.IO SETUP
+*
+*
+*/
+const socket = io('https://wss-space.glitch.me:30000'); //io('http://localhost:3000');
 let stars = [];
 let selfStar = null;
+let constellations = [];
+let loadedShip = null;
 
 socket.on('star-connected', data => {
     selfStar = data[data.length - 1];
     scene.activeCamera.position = new BABYLON.Vector3(selfStar.x, selfStar.y, selfStar.z);
     stars = data;
-    stars.splice(stars.length -1, 1);
+    //stars.splice(stars.length -1, 1);
     console.log(`you (${selfStar.id}) are now connected`);
     console.log(stars);
     stars.forEach(element => {
@@ -26,7 +34,23 @@ socket.on('star-disconnected', usrId => {
     console.log(`star ${usrId} disconnected`);
 });
 
-/* BABYLON SETUP */
+socket.on('ud-const-info', consts => {
+   constellations = consts;
+});
+
+socket.on('draw-consts', () => {
+    for(c = 0; c < constellations.length; c++){
+        drawConstellation(constellations[c]);
+    }
+});
+
+/*
+*
+*
+BABYLON SETUP
+*
+*
+*/
 const canvas = document.getElementById('renderCanvas');
 const engine = new BABYLON.Engine(canvas, true);
 
@@ -48,10 +72,55 @@ window.addEventListener('resize', function() { //resize the window
     engine.resize();
 });
 
-/* PLANETS APP */
+scene.beforeRender = function () {
+    if(loadedShip != null)
+        loadedShip.rotation.y += .1;  
+    //scene.activeCamera.position.x += .001;
+}
+
+/*
+*
+ASSET MANAGER - SHIP
+*
+*/
+let addShip = () => {
+    let assetManager = new BABYLON.AssetsManager(scene);
+    let shipTask = assetManager.addMeshTask('ship-task', '', 'models/', 'saucer.glb');
+    shipTask.onSuccess = (task) => {
+        console.log('peyi');
+        task.loadedMeshes[0].position = new BABYLON.Vector3(0, 0, 10);
+    };
+    shipTask.onError = (task, msg, exc) => {
+        console.log(msg, exc);
+    };
+    assetManager.load();
+    loadedShip = getMesh('__root__');
+};
+
+/*
+*
+*
+PLANETS APP
+*
+*
+*/
+//sets the scene ambient color
+scene.ambientColor = new BABYLON.Color3(1, 1, 1);
+//material for the stars
+let getStarMaterial = () => {
+    let mat = new BABYLON.StandardMaterial("starMaterial", scene);  
+    mat.diffuseColor = new BABYLON.Color3(1, 1, 1);
+    mat.specularColor = new BABYLON.Color3(Math.random(), Math.random(), Math.random());
+    mat.emissiveColor = new BABYLON.Color3(Math.random(), Math.random(), Math.random());
+    mat.ambientColor = new BABYLON.Color3(1, 1, 1);
+    mat.alpha = 1;
+    return mat;
+};
+
 let createStar = function(id, r, x, y, z) {
-    let sphere = BABYLON.MeshBuilder.CreateSphere(id, {segments: 8, diameter: r}, scene);
+    let sphere = BABYLON.MeshBuilder.CreateSphere(id, {segments: 4, diameter: r}, scene);
     sphere.position = new BABYLON.Vector3(x, y, z);
+    sphere.material = getStarMaterial();
 }
 
 let deleteStar = id => {
@@ -63,9 +132,88 @@ let deleteStar = id => {
     }
     for(i = 0; i < scene.meshes.length; i++) { //Deletes from BabylonJS scene (scene.meshes[])
         if(scene.meshes[i].id = id) {
-            scene.meshes.splice(i, 1);
+            delStar = scene.meshes.splice(i, 1);
+            if(delStar.length > 0) { delStar[0].dispose(); }
             break;
         }
     }
-    
 };
+
+// Skybox
+let skybox = BABYLON.MeshBuilder.CreateBox("skyBox", {size:200.0}, scene);
+var skyboxMaterial = new BABYLON.StandardMaterial("skyBox", scene);
+skyboxMaterial.backFaceCulling = false;
+var files = [
+    "imgs/bg/space_left.jpg",
+    "imgs/bg/space_up.jpg",
+    "imgs/bg/space_front.jpg",
+    "imgs/bg/space_right.jpg",
+    "imgs/bg/space_down.jpg",
+    "imgs/bg/space_back.jpg",
+];
+skyboxMaterial.reflectionTexture = new BABYLON.CubeTexture.CreateFromImages(files, scene);
+skyboxMaterial.reflectionTexture.coordinatesMode = BABYLON.Texture.SKYBOX_MODE;
+skyboxMaterial.disableLighting = true;
+skybox.material = skyboxMaterial;
+
+// time the constellation will be visible
+let constellationTime = 10000; //10s
+
+//auxiliary method to help transform position date from stars into drawing points for the mesh builder
+let createConstellationMeshData = (listStars) => {
+    let constell = [];
+    listStars.forEach((s)=>{
+        constell.push(new BABYLON.Vector3(s.x, s.y, s.z));
+    });
+    return constell;
+};
+
+//draws a constellation based on a list of points 
+let drawConstellation = (listStars) => {
+    let constellPoints = createConstellationMeshData(listStars);
+    let constName = `const-${Date.now()}`;
+    let constellation = BABYLON.MeshBuilder.CreateLines(constName, {points: constellPoints}, scene);
+    
+    var promConstellation = new Promise(function(resolve, reject) {
+        setTimeout(function() {
+            resolve();
+        }, constellationTime);
+    });
+
+    promConstellation.then(function(value) {
+        for (cm = 0; cm < scene.meshes.length; cm++) { //removes it from the meshes
+            if (scene.meshes[cm].name == constName) {
+                delConst = scene.meshes.splice(cm, 1);
+                delConst.dispose();
+                break;
+            }
+        }
+    });
+};
+
+//registers star into a constellation
+canvas.addEventListener('dblclick', (e) => {
+    socket.emit('reg-star-in-const', selfStar.id);
+});
+
+//retrieves a reference to a star from the collection
+let getStar = id => {
+    for(s = 0; s < stars.length; s++) {
+        if(stars[s].id = id){
+            return stars[s];
+        }
+    }
+};
+
+let getMesh = (id) => {
+    let mesh = null;
+    for(m = 0; m < scene.meshes.length; m++) {
+        if(scene.meshes[m].id == id) {
+            mesh = scene.meshes[m];
+            break; 
+        }
+    }
+    return mesh;
+};
+
+addShip();
--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const io = require('socket.io')(3000); //npm run devStart
 
 io.on('connection', socket => {
-    let protostar = createStar(socket.id);
-    stars.push(protostar);
+    let newStar = createStar(socket.id);
+    stars.push(newStar);
     console.log(`star ${socket.id} connected. # Stars: ${stars.length}`);
     socket.emit('star-connected', stars); //sends existing stars INCLUDING self to paint
 
-    socket.broadcast.emit('new-star-joined', protostar); //lets others know theres a new star to create
+    socket.broadcast.emit('new-star-joined', newStar); //lets others know theres a new star to create
 
     socket.on('disconnect', () => {
         socket.broadcast.emit('star-disconnected', socket.id);
@@ -14,22 +14,41 @@ io.on('connection', socket => {
         console.log(`star ${socket.id} disconnected. # Stars: ${stars.length}`);
     });
 
+    socket.on('reg-star-in-const', (m) => { //adds a star to a constellation
+        if(formingConstellation) {
+            currentConstellation.push(getStar(m));
+        }
+    });
+
+    let drawConstellations = () => {
+        socket.broadcast.emit('draw-consts', ''); //tells clients to draw the constellations
+    };
+    
+    let updateConstellationInfo = () => {
+        socket.broadcast.emit('ud-const-info', constellations); //updates the constellation information in the client
+        console.log('broadcasting constellations');
+    };
 });
+
+
 
 /* SPACE SETUP */
 let xConfines = 20;
 let yConfines = 20;
 let zConfines = 20;
-let initialMaxRadius = .7;
+let initialMaxRadius = .4;
 let stars = []; //contains all stars in the scene
+let constellations = [];
+let currentConstellation = [];
+let formingConstellation = false;
 
 let createStar = id => {
     let newStarData = {
         id: id,
         r: Math.random() * initialMaxRadius,
-        x: Math.random() * xConfines,
-        y: Math.random() * yConfines,
-        z: Math.random() * zConfines}
+        x: Math.random() * xConfines * numSign(),
+        y: Math.random() * yConfines  * numSign(),
+        z: Math.random() * zConfines * numSign()}
     return newStarData;
 };
 
@@ -40,4 +59,9 @@ let deleteStar = id => {
             break;
         } 
     }
+};
+
+let numSign = () => {
+    s = Math.random() > 0.5? 1 : -1;
+    return s;
 };
